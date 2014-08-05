b297e2cda152f5b1429673dbbe1119b36920f22d
David Rundle <dave@dbones.co.uk>
2014-08-05 23:46:31 +0100

    example code

---
+++ src/Flickr2/App/viewmodels/flickr.js
@@ -0,0 +1,22 @@
+﻿define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+    //Note: This module exports an object.
+    //That means that every module that "requires" it will get the same object instance.
+    //If you wish to be able to create multiple instances, instead export a function.
+
+    return {
+        searchTag : ko.observable(""),
+        search: function () {
+            var that = this;
+            return http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne', { tags: that.searchTag(), tagmode: 'any', format: 'json' }, 'jsoncallback').then(function(response) {
+                that.images(response.items);
+            });
+        },
+        displayName: 'Flickr',
+        images: ko.observableArray([]),
+        activate: function () { this.search(); },
+        select: function(item) {
+            //AJAX :)
+            http.post('/Images', { id: item.link });
+        },
+    };
+});
---
+++ src/Flickr2/HomeModule.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Conventions;
+using Nancy.ModelBinding;
+
+namespace Flickr2
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["index.html"];
+        }
+
+    }
+
+    public static class MemoryDb
+    {
+        static MemoryDb()
+        {
+            Ids = new List<string>();
+        }
+
+        public static IList<string> Ids { get; set; }
+
+    }
+
+    public class ImagesModule : NancyModule
+    {
+        public ImagesModule()
+            : base("/images")
+        {
+            Post["/"] = _ =>
+                {
+
+                    var image = this.Bind<Image>();
+
+                    MemoryDb.Ids.Add(image.Id);
+                    return HttpStatusCode.OK;
+                };
+
+
+            Get["/"] = _ =>
+                {
+                    return MemoryDb.Ids;
+                };
+        }
+    }
+
+
+    public class Image
+    {
+        public string Id { get; set; }
+    }
+
+
+    public class ApplicationBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("App"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Content"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("fonts"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Images"));
+            base.ConfigureConventions(nancyConventions);
+        }
+
+        protected override void RequestStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines, NancyContext context)
+        {
+            //CORS Enable
+            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                                .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                                .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+
+            });
+        }
+
+    }
+}
